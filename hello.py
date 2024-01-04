b00ba7ea73c61303b9c5dc60367e5c32fa1016d7
inovaski <gnovaskii@outlook.com>
2024-01-04 14:41:32 -0300

    Implementando Hello World

--- hello.py
+++ hello.py
@@ -27,5 +27,9 @@ if current_language =="pt_BR":
     msg = "Olá Mundo!"
 elif current_language == "es_EP":
     msg == "Hola Mundo"
+elif current_language == "it_IT":
+    msg == "Ciao Mondo"
+elif current_language == "fr_FR":
+    msg == "Bonjour Monde"
 
 print (msg)
